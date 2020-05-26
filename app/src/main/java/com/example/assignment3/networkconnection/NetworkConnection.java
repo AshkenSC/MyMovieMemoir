fb0976db70c0675fcae38977c210e4103df165c7
AshkenSC <393940378@qq.com>
2020-05-26 23:54:03 +0800

    BASE_URL has been fixed in NetworkConnection.java. The program can now get access to the database correctly.

--- app/src/main/java/com/example/assignment3/networkconnection/NetworkConnection.java
+++ app/src/main/java/com/example/assignment3/networkconnection/NetworkConnection.java
@@ -1,4 +1,34 @@
 package com.example.assignment3.networkconnection;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class NetworkConnection {
+    private OkHttpClient client=null;
+    private String results;
+    public static final MediaType JSON =
+            MediaType.parse("application/json; charset=utf-8");
+    public NetworkConnection(){
+        client=new OkHttpClient();
+    }
+    private static final String BASE_URL =
+            "http://10.0.2.2:8080/Assignment1/webresources/";
+
+    public String getAllPersons(){
+        final String methodPath = "restws.person/";
+        Request.Builder builder = new Request.Builder();
+        builder.url(BASE_URL + methodPath);
+        Request request = builder.build();
+        try {
+            Response response = client.newCall(request).execute();
+            results=response.body().string();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return results;
+    }
+
 }
+
