f609685bc740812b7dbd317a48acba65cf455992
AshkenSC <393940378@qq.com>
2020-05-30 12:08:11 +0800

    Bug fix: SharedViewModel class is now placed and can be imported correctly.

---
+++ app/src/main/java/com/example/assignment3/SharedViewModel.java
@@ -0,0 +1,16 @@
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SharedViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+    public SharedViewModel(){
+        mText = new MutableLiveData<>();
+    }
+    public void setMessage(String message) {
+        mText.setValue(message);
+    }
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
